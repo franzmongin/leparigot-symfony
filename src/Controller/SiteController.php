203aba524b824a9204db5eef5735b5631e1bea72
franzmongin <franz.mongin@gmail.com>
2019-02-11 23:02:07 +0100

    show article page

--- src/Controller/SiteController.php
+++ src/Controller/SiteController.php
@@ -4,6 +4,9 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+
+use App\Entity\Article;
 
 class SiteController extends AbstractController
 {
@@ -14,6 +17,17 @@ class SiteController extends AbstractController
     {
         return $this->render('site/home.html.twig', [
             'controller_name' => 'SiteController',
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="article_show")
+     * 
+     */
+    public function show(Article $article)
+    {
+        return $this->render('site/show.html.twig', [
+            'article' => $article
         ]);
     }
 
